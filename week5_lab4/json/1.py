8bf283b46e8588caf1f7fc48272d89115f04d74f
DiasAdiletuly <123188898+DiasAdiletuly@users.noreply.github.com>
2023-02-19 21:26:19 +0500

    Update 1.py

--- week5_lab4/json/1.py
+++ week5_lab4/json/1.py
@@ -1,17 +1,14 @@
 import json
-import requests
-
-response = requests.get("https://raw.githubusercontent.com/erlanurbergen/pp2/master/week5/Lab4/json/sample-data.json")
-data = json.loads(response.text)
-    # print(data)
-print("""Interface Status================================================================================""")
+with open("sample-data.json", "r") as read_file:
+    data = json.load(read_file)
+print(data)
+print("""Interface Status
+================================================================================""")
 print("""DN                                             Description          Speed                      MTU """)
-for imdata in data["imdata"]:
-    for i, k in imdata['l1PhysIf']["attributes"].items():
-        if i == 'dn':
-            print(k, end="                          ")
-        if i == "speed":
-            print(k, end="                                         ")
-        if i == "mtu":
-            print(k, end="                   ")
-    print()
+for i, k in data["imdata"][0]['l1PhysIf']["attributes"].items():
+    if i == 'dn':
+        print(k, end="                          ")
+    if i == "speed":
+        print(k, end="                                         ")
+    if i == "mtu":
+        print(k, end="                   ")
