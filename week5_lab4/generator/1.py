88e695ca7fa37c952113321c252668b99e79a455
DiasAdiletuly <123188898+DiasAdiletuly@users.noreply.github.com>
2023-03-07 10:33:05 +0500

    1

--- week5_lab4/generator/1.py
+++ week5_lab4/generator/1.py
@@ -1,6 +1,6 @@
 
 n = int(input("write number: "))
-sq_numbers = [i*i for i in range(1,n)]
-
+sq_numbers = (i*i for i in range(1,n))
+print(sq_numbers)
 for i in sq_numbers:
     print(i)
--- week5_lab4/generator/2.py
+++ week5_lab4/generator/2.py
@@ -1,5 +1,6 @@
 number = int(input("Write number: "))
-even_n = [i for i in range(number) if i%2==0]
+even_n = (i for i in range(number) if i%2==0)
 
 print("All even numbers up to {}".format(number))
 print(*even_n,sep =", ")
+print(even_n)
